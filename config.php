dc5ecf3e018931492d5b10dc44d258212614fe16
Adiputra Sinaga <32774236+adiputrasinaga@users.noreply.github.com>
2019-07-09 19:32:07 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $databaseHost = 'mysql-dicodingazure1.mysql.database.azure.com';
 $databaseName = 'dbdicodingazure';
 $databaseUsername = 'adiputra@mysql-dicodingazure1';
-$databasePassword = 'Sinaga17';
+$databasePassword = 'Nagasaon.007';
 
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
  
