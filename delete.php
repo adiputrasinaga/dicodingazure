15b3673e9ac48df496bf8620d7d8e7b975347aec
Adiputra Sinaga <32774236+adiputrasinaga@users.noreply.github.com>
2019-07-09 19:00:09 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -3,7 +3,7 @@ include("config.php");
 
 $id = $_GET['id'];
 //hapus data berdasarkan id
-$result = mysqli_query($mysqli, "DELETE FROM identitasku WHERE id=$id");
+$result = mysqli_query($mysqli, "DELETE FROM biodata WHERE id=$id");
 
 header("Location:index.php");
 ?>
