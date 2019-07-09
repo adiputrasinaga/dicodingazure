a418164bc30ab5b157a26039e3a4e0b2f32f42c9
Adiputra Sinaga <32774236+adiputrasinaga@users.noreply.github.com>
2019-07-09 18:58:17 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 //autentikasi
-$databaseHost = 'mysql-submission1.mysql.database.azure.com';
-$databaseName = 'dicodingquest';
-$databaseUsername = 'dragz17@mysql-submission1';
-$databasePassword = 'P@ssw0rd123!@#';
+$databaseHost = 'mysql-dicodingazure1.mysql.database.azure.com';
+$databaseName = 'dbdicodingazure';
+$databaseUsername = 'adiputra@mysql-dicodingazure1';
+$databasePassword = 'Sinaga17';
 
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
  
